3dbac721f248cf2f60a0cd7d7dec054922cfa8f3
BassamRamadan <bassam.ramadan.fci@gmail.com>
2025-08-09 17:54:40 +0300

    Remove unneeded code

--- platformService/Program.cs
+++ platformService/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
-using PlatformService.SyncDataToServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +11,6 @@ options.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddHttpClient<ICommandServiceClient, HttpCommandServiceClient>();
 
 var app = builder.Build();
 
