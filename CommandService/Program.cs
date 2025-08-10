aee47ff31688ae2a68ab0d0801486c02835b5fdb
BassamRamadan <bassam.ramadan.fci@gmail.com>
2025-08-10 12:15:20 +0300

    Updating Program with Setup

--- CommandService/Program.cs
+++ CommandService/Program.cs
@@ -1,6 +1,13 @@
+using CommandService.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext <AppDbContext>(Options => Options.UseInMemoryDatabase("InMemoryDb"));
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<ICommandRepo, CommandRepo>();
 
 var app = builder.Build();
 
