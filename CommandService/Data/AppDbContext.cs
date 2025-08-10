0f165cadea051b785dc091b5618b5448c7df46e9
BassamRamadan <bassam.ramadan.fci@gmail.com>
2025-08-10 12:08:57 +0300

    Creationg DbContext and defining the relationship between Command-Platform models

---
+++ CommandService/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+using CommandService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Command> Commands { get; set; }
+        public DbSet<Platform> Platforms { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Command>()
+                .HasOne(c => c.Platform)
+                .WithMany(p => p.Commands)
+                .HasForeignKey(c => c.PlatformId);
+        }
+    }
+}
