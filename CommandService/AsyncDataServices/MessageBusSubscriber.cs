081e8eaac18547c296a59c258bda5bc729ab2c5e
BassamRamadan <bassam.ramadan.fci@gmail.com>
2025-08-17 13:58:01 +0300

    Creating out MessageBus Subscriber

---
+++ CommandService/AsyncDataServices/MessageBusSubscriber.cs
@@ -0,0 +1,11 @@
+
+namespace CommandService.AsyncDataServices
+{
+    public class MessageBusSubscriber : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
